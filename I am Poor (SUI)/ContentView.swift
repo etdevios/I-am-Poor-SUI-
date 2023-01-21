6155f1f9d7caea405ed5886802d6d75669f58239
Eduard Tokarev <etdevios@gmail.com>
2023-01-22 00:43:23 +0300

    Initial Commit

---
+++ I am Poor (SUI)/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  I am Poor (SUI)
+//
+//  Created by Eduard Tokarev on 22.01.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ I am Poor (SUI)/I_am_Poor__SUI_App.swift
@@ -0,0 +1,17 @@
+//
+//  I_am_Poor__SUI_App.swift
+//  I am Poor (SUI)
+//
+//  Created by Eduard Tokarev on 22.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct I_am_Poor__SUI_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
